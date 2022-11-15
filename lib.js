c835a95a76796e619af3ed0c96ed116143cde600
Ggabriel-SS <97918580+Ggabriel-SS@users.noreply.github.com>
2022-11-15 17:59:46 -0300

    Update lib.js

--- lib.js
+++ lib.js
@@ -9,7 +9,14 @@ canvas.style.height = `${altura * 0.85}px`
 if(largura < 600){
     canvas.style.width = `${largura}px`
 }
-
+if(altura > 900){
+    canvas.style.height = `850px`
+    
+    if(altura > 600){
+    canvas.style.height = `550px`
+    canvas.style.width = `${1100/3}px`
+    }
+} 
 
 //Essa classe é a junção de 4, teremos que dividir entre mães e filhas
 //Classe mãe (Precisa de um nome)
